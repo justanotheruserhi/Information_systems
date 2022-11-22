e606baaf88ca7dfb149692d4f471c04c875562a0
Gubanova valeria <gubanovavaleria07@gmail.com>
2022-11-22 13:08:59 +0600

    connect with postgresql

---
+++ src/main/java/com/javapoint/habits/controller/ConnectPostgresql.java
@@ -0,0 +1,29 @@
+package com.javapoint.habits.controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectPostgresql {
+
+    private final String url = "jdbc:postgresql://localhost:5432/habits";
+    private final String user = "habits_user";
+    private final String password = "password";
+
+    /**
+     * Connect to the PostgreSQL database
+     *
+     * @return a Connection object
+     */
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+}
