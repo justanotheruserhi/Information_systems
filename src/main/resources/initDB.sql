aaca5e091e7c34f08cb5d2a0113f00cdd393107b
rina_the_fish <yuoturellepikie@gmail.com>
2022-11-23 00:16:47 +0600

    created and fixed databases

--- src/main/resources/initDB.sql
+++ src/main/resources/initDB.sql
@@ -1,27 +1,27 @@
-create table user_data (
-	user_id integer(3)not null primary key,
-	username varchar(20)not null unique,
+create table if not exists user_data (
+	user_id integer not null primary key,
+	username varchar (20)not null unique,
 	password varchar (10) not null,
 	birthday date not null
 );
 
-create table habits_data (
-	habit_id integer (4) not null primary key,
+create table if not exists habits_data (
+	habit_id integer not null primary key,S
 	habit_desc varchar (255) not null,
 	habit_color integer (5)
 );
 
-create table positive_notifs (
-	note_pos_id integer (6) not null primary key,
+create table if not exists positive_notifs (
+	note_pos_id integer not null primary key,
 	note_pos_txt varchar (30) not null
 );
 
-create table negative_notifs (
-	note_neg_id integer (6) not null primary key,
+create table if not exists negative_notifs (
+	note_neg_id integer not null primary key,
 	note_neg_txt varchar (30) not null
 );
 
-create table friends_data (
+create table if not exists friends_data (
 	not null foreign key (user_id)
 		references user_data (user_id),
 	not null foreign key (friend_id)
@@ -30,7 +30,7 @@ create table friends_data (
 		references user_data (user_id)
 );
 
-create table user_check_in (
+create table if not exists user_check_in (
 	not null foreign key (user)
 		references user_data (user_id),
 	not null foreign key (habit)
@@ -38,10 +38,10 @@ create table user_check_in (
 	check_date timestamp
 );
 
-create table statistics (
+create table if not exists statistics (
     not null foreign key (user_id)
         references user_data (user_id),
-    cnt_habits integer (10) not null,
+    cnt_habits integer not null,
     date_start date not null,
     date_end date not null,
-)
+);
